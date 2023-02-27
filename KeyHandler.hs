f5a77e1c5057dae58c0cf6a417df48d7a04a849f
Andrew Cheah <62257557+atcheah@users.noreply.github.com>
2023-02-26 17:39:25 -0800

    level up monster w/in levelUpScene

--- KeyHandler.hs
+++ KeyHandler.hs
@@ -31,25 +31,26 @@ handleLevelUpClick :: Float -> Float -> World -> World
 handleLevelUpClick x y w =
   do
     let hero = getHero (internalState w)
+    let newMonster = autoLevelUpCharacter (getMonster (internalState w))
     let newRound = getRound (internalState w) + 1
     -- health button
     if (x > (-350) && x < (-150) && y > (-25) && y < 25) then
-      (World "fight" (seconds w) (IhcnternalState (levelUpCharacter hero (Action 1)) (getMonster (internalState w)) newRound))
+      (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 1)) newMonster newRound))
     else 
       -- attack button
       if (x > (-100) && x < 100 && y > (-25) && y < 25) then 
-        (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 2)) (getMonster (internalState w)) newRound))
+        (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 2)) newMonster newRound))
       else
         -- bleed button
         if (x > 150 && x < 350 && y > (-25) && y < 25) then
-          (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 3)) (getMonster (internalState w)) newRound))
+          (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 3)) newMonster newRound))
         else
           -- life steal button
           if (x > (-225) && x < (-25) && y > (-125) && y < (-75)) then
-            (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 4)) (getMonster (internalState w)) newRound))
+            (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 4)) newMonster newRound))
           else
             -- priority button
             if (x > 25 && x < 225 && y > (-125) && y < (-75)) then
-              (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 5)) (getMonster (internalState w)) newRound))
+              (World "fight" (seconds w) (InternalState (levelUpCharacter hero (Action 5)) newMonster newRound))
             else
               w
